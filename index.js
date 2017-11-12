7a62105f8d23044cec22d53b0fbf52dcd0d8cafe
Angelo Cigna <acigna1982@gmail.com>
2017-11-12 00:13:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ const driver = {
 function updateDriverWithKeyAndValue(driver, key, value) {
   return object.assign({}, driver {[key]: value});
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign(driver, {[key]: value});
+}
+
+function deleteFromDriverByKey(driver, key) {
+  const newdriver = Object.assign({}, driver);
+  delete newdriver[key];
+  return newdriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  return delete driver[key];
